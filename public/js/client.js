3cd0c90d68502e83724de4893fb324d2591d91b5
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-19 10:35:46 -0500

    client js updates

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,24 @@
 angular.module('twitterApp', []);
 
-angular.module('twitterApp').controller('MainController', function($http){
-
+angular.module('twitterApp').controller('MainController', function($scope, $http){
+    //$http get info
+    //random adjective
+    //random noun
+    $scope.handleClick = function() {
+      $http.get('/adj')
+      .then(function(response){
+        var ranNum = (Math.floor(Math.random() * 10));
+        console.log(response);
+        //$scope.adjective = response.data.
+      });
+      $http.get('/noun')
+      .then(function(response){
+        var ranNum = (Math.floor(Math.random() * 10));
+        $scope.noun = response.data.ourAdjectives[ranNum];
+      });
+      //combine adj+noun
+      $scope.twitterHandle = $scope.adjective + $scope.noun;
+  }
 
 
 })
