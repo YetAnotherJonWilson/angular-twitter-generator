3c487176329024429d4b048bfe1262e6afe63f9c
Andrew Kolander <akolander22@gmail.com>
2016-07-19 10:31:15 -0500

    html updates

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,6 @@
 angular.module('twitterApp', []);
 
-angular.module('twitterApp').controller('MainController', function($http){
-
+angular.module('twitterApp').controller('MainController', function($scope,$http){
 
 
 })
