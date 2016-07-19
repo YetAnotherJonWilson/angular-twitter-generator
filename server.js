5d459f0ff75b3fb3a215e2c9e02ac2aaa28b2dc4
Andrew Kolander <akolander22@gmail.com>
2016-07-19 09:36:07 -0500

    minor updates

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var index = require('/public.index.html');
+var index = require('./routes/index');
 var app = express();
 
 
-app.use('/');
-
 app.use(express.static('public'));
+
+app.use('/', index);
+
+
+
 
 var server = app.listen(process.env.PORT || 3000, function(){
   var port = server.address().port;
